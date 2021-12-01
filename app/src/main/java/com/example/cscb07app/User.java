26841e632ededa94cd823bc48f0e73efa5f41f62
JohnP <phamtuanky@gmail.com>
2021-11-30 22:28:52 -0500

    Change to Manifest to fix return issue

--- app/src/main/java/com/example/cscb07app/User.java
+++ app/src/main/java/com/example/cscb07app/User.java
@@ -22,6 +22,11 @@ public class User {
         this.email = email;
     }
 
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
     public String getUsername() {
         return username;
     }
