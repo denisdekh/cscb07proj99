59d42f41ec503497c97bd2d03d49ee0c43a79a17
Kieran <kieran.r.hansen@gmail.com>
2021-12-06 03:41:04 -0500

    Created activity to view past orders of customers
    Plenty of changes, changed how total cost of an order is sent to the database. Created activites and xmls for customers to view past orders. Will be further extended upon.

---
+++ app/src/main/java/com/example/cscb07app/customer/viewOrders/CustomerViewOrders.java
@@ -0,0 +1,142 @@
+package com.example.cscb07app.customer.viewOrders;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.renderscript.Sampler;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cscb07app.R;
+import com.example.cscb07app.customer.CustomerHomeActivity;
+import com.example.cscb07app.login.LoginModel;
+import com.example.cscb07app.order.Order;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerViewOrders extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    ArrayList<String> orderIds;
+    ArrayList<String> storeNames;
+    ArrayList<String> totalPrices;
+    ArrayList<String> orderStatuses;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_view_orders);
+
+        Intent i = getIntent();
+        String username = i.getStringExtra(LoginModel.USERNAME);
+
+        orderIds = new ArrayList<>();
+        storeNames = new ArrayList<>();
+        totalPrices = new ArrayList<>();
+        orderStatuses = new ArrayList<>();
+
+        populateLists(username);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        ViewOrdersAdapter adapter = new ViewOrdersAdapter(orderIds, storeNames, totalPrices, orderStatuses, this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    public void populateLists(String username){
+        //Creating a reference to the database
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot child: snapshot.child("Account").child("Customer").child(username).child("orders").getChildren()){
+                    String orderId = child.getValue().toString();
+                    String storeId = snapshot.child("Orders").child(orderId).child("storeId").getValue().toString();
+
+                    //set the parameters for a new order that we will add to the list of the orders at the specified position
+                    String storeName = snapshot.child("Stores").child(storeId).child("name").getValue().toString();
+                    String totalPrice = snapshot.child("Orders").child(orderId).child("price").getValue().toString();
+                    String orderStatus;
+                    if((Boolean)snapshot.child("Orders").child(orderId).child("completed").getValue()){
+                        orderStatus = "Ready for Pickup!";
+                    } else{
+                        orderStatus = "Currently Being Prepared!";
+                    }
+
+                    insertLists(orderId, storeName, totalPrice, orderStatus);
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w("warning", "loadPost:onCancelled", error.toException());
+            }
+        };
+
+        ref.addValueEventListener(listener);
+    }
+
+    public void insertLists(String id, String store, String price, String status){
+        //The purpose of this method is to insert the items into the list reverse chronologically
+        //this mean that an order with id o11 would be inserted after order o32
+
+        //id.substring 1 gives the id without the o identifier.
+        int orderNumber = Integer.parseInt(id.substring(1));
+
+        if(orderIds.size() == 0){
+            orderIds.add(id);
+            storeNames.add(store);
+            totalPrices.add(price);
+            orderStatuses.add(status);
+        }
+        else if(orderIds.size() == 1){
+            if(Integer.parseInt(orderIds.get(0).substring(1)) > orderNumber){
+                orderIds.add(id);
+                storeNames.add(store);
+                totalPrices.add(price);
+                orderStatuses.add(status);
+            } else {
+                orderIds.add(0, id);
+                storeNames.add(0, store);
+                totalPrices.add(0, price);
+                orderStatuses.add(0, status);
+            }
+        }
+        else { //size of the list is minimum 2, and so the following insertion algorithm should work
+            if(Integer.parseInt(orderIds.get(0).substring(1)) < orderNumber){ //if the id number is higher than every other one, it is the newest in the system -> attach to start
+                orderIds.add(0, id);
+                storeNames.add(0, store);
+                totalPrices.add(0, price);
+                orderStatuses.add(0, status);
+            } else if(Integer.parseInt(orderIds.get(orderIds.size()-1).substring(1)) > orderNumber){ //the id at the end of the array is smaller -> oldest -> attach to end
+                orderIds.add(id);
+                storeNames.add(store);
+                totalPrices.add(price);
+                orderStatuses.add(status);
+            } else {
+                for (int i = 0; i < orderIds.size() - 1; i++) { //comparing up to size - 1 gives all the characters while avoiding null pointer
+                    if (Integer.parseInt(orderIds.get(i).substring(1)) > orderNumber && Integer.parseInt(orderIds.get(i+1).substring(1)) < orderNumber) {
+                        orderIds.add(i+1, id);
+                        storeNames.add(i+1, store);
+                        totalPrices.add(i+1, price);
+                        orderStatuses.add(i+1, status);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cscb07app/customer/viewOrders/ViewOrdersAdapter.java
@@ -0,0 +1,67 @@
+package com.example.cscb07app.customer.viewOrders;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cscb07app.R;
+
+import java.util.ArrayList;
+
+public class ViewOrdersAdapter extends RecyclerView.Adapter<ViewOrdersAdapter.MyViewHolder>{
+
+    ArrayList<String> orderIds;
+    ArrayList<String> storeNames;
+    ArrayList<String> totalPrices;
+    ArrayList<String> orderStatuses;
+    Context context;
+
+    public ViewOrdersAdapter(ArrayList<String> orderIds, ArrayList<String> storeNames, ArrayList<String> totalPrices, ArrayList<String> orderStatuses, Context context) {
+        this.orderIds = orderIds;
+        this.storeNames = storeNames;
+        this.totalPrices = totalPrices;
+        this.orderStatuses = orderStatuses;
+        this.context = context;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView storeView;
+        TextView idView;
+        TextView statusView;
+        TextView priceView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            idView = itemView.findViewById(R.id.OrderIdTextView);
+            storeView = itemView.findViewById(R.id.StoreNameTextView);
+            statusView = itemView.findViewById(R.id.StatusTextView);
+            priceView = itemView.findViewById(R.id.PriceTextView);
+        }
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.idView.setText(orderIds.get(position));
+        holder.storeView.setText(storeNames.get(position));
+        holder.statusView.setText(orderStatuses.get(position));
+        holder.priceView.setText(totalPrices.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return orderIds.size();
+    }
+}
